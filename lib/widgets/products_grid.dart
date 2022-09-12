040edc89b8cb6343c195709481456e001d5172b9
medansatu <saifulanshor48@gmail.com>
2022-09-13 06:58:06 +0700

    initial release

---
+++ lib/widgets/products_grid.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products_provider.dart';
+import './product_item.dart';
+
+class ProductsGrid extends StatelessWidget {
+  final bool showFavs;
+
+  ProductsGrid(this.showFavs);
+
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context);
+    final products =
+        showFavs ? productsData.favoriteItems : productsData.items;
+    return GridView.builder(
+      padding: const EdgeInsets.all(10),
+      itemCount: products.length,
+      itemBuilder: (context, index) => ChangeNotifierProvider.value(
+        // create: (ctx) => products[index],
+        value: products[index],
+        child: ProductItem(
+            // products[index].id,
+            // products[index].title,
+            // products[index].imageUrl,
+            // products[index].price,
+            ),
+      ),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 2 / 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+    );
+  }
+}
